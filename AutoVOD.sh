4e54da5a44d62ab4c4fe7d1b78623abd78012255
Jens <69081683+jenslys@users.noreply.github.com>
2022-01-31 12:55:43 +0100

    fixed logging var

--- AutoVOD.sh
+++ AutoVOD.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting AutoVOD..."
-echo "Using Twitch user: $TWITCH_USER"
+echo "Using Twitch user: $2"
 echo ""
 echo ""
 
@@ -12,10 +12,10 @@ do
 	# Define some variables
 	STREAMER_NAME=$2 # Dont change this
 	TIME_DATE=[$(date +"%m.%d.%y")] # [08.10.21]
-	VIDEO_VISIBILITY="unlisted" # unlisted, private, public
+	VIDEO_VISIBILITY="unlisted" # options: unlisted, private, public
 	VIDEO_DESCRIPTION="Watch $STREAMER_NAME live on https://twitch.tv/$STREAMER_NAME \n\nUploaded using https://github.com/jenslys/AutoVOD"
-	VIDEO_TITLE=$TIME_DATE
-	VIDEO_DURATION="12:00:00" # XX:XX:XX (Youtube has a upload limit set to 12 hours per video)
+	VIDEO_TITLE=$TIME_DATE # Set to whatever you want
+	VIDEO_DURATION="12:00:00" # XX:XX:XX (YouTube has a upload limit at 12 hours per video)
 	SPLIT_INTO_PARTS="true" # If you want to split the videos into multiple parts. (if this is enabled, VIDEO_DURATION is ignored)
 	SPLIT_VIDEO_DURATION="06:00:00"
 	if [[ "$SPLIT_INTO_PARTS" == "true" ]]; then
