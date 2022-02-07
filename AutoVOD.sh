1601a4962c8d58b40d4cddbeb664c67a3b41444b
Jens <69081683+jenslys@users.noreply.github.com>
2022-02-07 09:11:08 +0100

    formatting

--- AutoVOD.sh
+++ AutoVOD.sh
@@ -8,27 +8,26 @@ echo ""
 # Every minute, try to download the Twitch stream, and send it to YouTube.
 # Everything through the pipe, no video file is created.
 
-while true
-do
+while true; do
 	# Variables
-	STREAMER_NAME=$2 #! Dont change this
-	TIME_DATE=[$(date +"%m.%d.%y")] # Example: [08.10.21]
-	VIDEO_VISIBILITY="unlisted" #* Options: unlisted, private, public
+	STREAMER_NAME=$2                                                                                                                        #! Dont change this
+	TIME_DATE=[$(date +"%m.%d.%y")]                                                                                                         # Example: [08.10.21]
+	VIDEO_VISIBILITY="unlisted"                                                                                                             #* Options: unlisted, private, public
 	VIDEO_DESCRIPTION="Watch $STREAMER_NAME live on https://twitch.tv/$STREAMER_NAME \n\nUploaded using https://github.com/jenslys/AutoVOD" # YouTube video description.
-	VIDEO_TITLE="$STREAMER_NAME - $TIME_DATE" # Title of the Youtube video.
-	VIDEO_DURATION="12:00:00" # XX:XX:XX (YouTube has a upload limit of 12 hours per video).
-	VIDEO_PLAYLIST="$STREAMER_NAME VODs" # Playlist to upload to.
-	SPLIT_INTO_PARTS="false" # If you want to split the video into parts, set this to true. (if this is enabled VIDEO_DURATION is ignored).
+	VIDEO_TITLE="$STREAMER_NAME - $TIME_DATE"                                                                                               # Title of the Youtube video.
+	VIDEO_DURATION="12:00:00"                                                                                                               # XX:XX:XX (YouTube has a upload limit of 12 hours per video).
+	VIDEO_PLAYLIST="$STREAMER_NAME VODs"                                                                                                    # Playlist to upload to.
+	SPLIT_INTO_PARTS="false"                                                                                                                # If you want to split the video into parts, set this to true. (if this is enabled VIDEO_DURATION is ignored).
 	SPLIT_VIDEO_DURATION="06:00:00"
 
 	# Splitting the stream into parts (If enabled)
 	if [[ "$SPLIT_INTO_PARTS" == "true" ]]; then
 		VIDEO_DURATION=$SPLIT_VIDEO_DURATION
-		if [[ "$TIME_DATE" == "$TIME_DATE_CHECK"  ]]; then
-			CURRENT_PART=$(( $CURRENT_PART + 1 ))
+		if [[ "$TIME_DATE" == "$TIME_DATE_CHECK" ]]; then
+			CURRENT_PART=$(($CURRENT_PART + 1))
 			VIDEO_TITLE="$VIDEO_TITLE - Part $CURRENT_PART"
 		else
-		  CURRENT_PART=1
+			CURRENT_PART=1
 		fi
 	fi
 
@@ -37,7 +36,7 @@ do
 	echo "Checking twitch.tv/$STREAMER_NAME for a stream."
 
 	# Create the input file with upload parameters
-	echo '{"title":"'"$VIDEO_TITLE"'","privacyStatus":"'"$VIDEO_VISIBILITY"'","description":"'"$VIDEO_DESCRIPTION"'","playlistTitles":["'"${VIDEO_PLAYLIST}"'"]}' > /tmp/input.$STREAMER_NAME
+	echo '{"title":"'"$VIDEO_TITLE"'","privacyStatus":"'"$VIDEO_VISIBILITY"'","description":"'"$VIDEO_DESCRIPTION"'","playlistTitles":["'"${VIDEO_PLAYLIST}"'"]}' >/tmp/input.$STREAMER_NAME
 
 	# Start StreamLink and YoutubeUploader
 	streamlink twitch.tv/$STREAMER_NAME $STREAMLINK_OPTIONS | youtubeuploader -metaJSON /tmp/input.$STREAMER_NAME -filename - >/dev/null 2>&1 && TIME_DATE_CHECK=$TIME_DATE
