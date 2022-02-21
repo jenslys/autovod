c37952d06741ca50f945cb4b634d26da89520c47
Jens <69081683+jenslys@users.noreply.github.com>
2022-02-21 16:09:11 +0100

    Changed the default description

--- AutoVOD.sh
+++ AutoVOD.sh
@@ -9,15 +9,14 @@ echo ""
 # Everything through the pipe, no video file is created.
 
 while true; do
-	# Variables
-	STREAMER_NAME=$2                                                                                                                        #! Dont change this
-	TIME_DATE=[$(date +"%m.%d.%y")]                                                                                                         # Example: [08.10.21]
-	VIDEO_VISIBILITY="unlisted"                                                                                                             #* Options: unlisted, private, public
-	VIDEO_DESCRIPTION="Watch $STREAMER_NAME live on https://twitch.tv/$STREAMER_NAME \n\nUploaded using https://github.com/jenslys/AutoVOD" # YouTube video description.
-	VIDEO_TITLE="$STREAMER_NAME - $TIME_DATE"                                                                                               # Title of the Youtube video.
-	VIDEO_DURATION="12:00:00"                                                                                                               # XX:XX:XX (YouTube has a upload limit of 12 hours per video).
-	VIDEO_PLAYLIST="$STREAMER_NAME VODs"                                                                                                    # Playlist to upload to.
-	SPLIT_INTO_PARTS="false"                                                                                                                # If you want to split the video into parts, set this to true. (if this is enabled VIDEO_DURATION is ignored).
+	STREAMER_NAME=$2                                                      #! Dont change this, this value is passed when starting the script.
+	TIME_DATE=[$(date +"%m.%d.%y")]                                       # Preview example: [08.10.21]
+	VIDEO_VISIBILITY="unlisted"                                           #* Options: unlisted, private, public
+	VIDEO_DESCRIPTION="Uploaded using https://github.com/jenslys/AutoVOD" # YouTube video description.
+	VIDEO_TITLE="$STREAMER_NAME - $TIME_DATE"                             # Title of the Youtube video.
+	VIDEO_DURATION="12:00:00"                                             # XX:XX:XX (YouTube has a upload limit of 12 hours per video).
+	VIDEO_PLAYLIST="$STREAMER_NAME VODs"                                  # Playlist to upload to.
+	SPLIT_INTO_PARTS="false"                                              # If you want to split the video into parts, set this to true. (if this is enabled VIDEO_DURATION is ignored).
 	SPLIT_VIDEO_DURATION="06:00:00"
 
 	# Splitting the stream into parts (If enabled)
