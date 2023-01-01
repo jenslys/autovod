016849d3a30c815769a2328ecb2304672049e92f
Jens <69081683+jenslys@users.noreply.github.com>
2023-01-01 14:20:44 +0100

    refactoring
    - Refactor checking for variables and fetching stream meta
    - Refactor checking for config files

--- AutoVOD.sh
+++ AutoVOD.sh
@@ -29,14 +29,12 @@ config_file="$STREAMER_NAME.config"
 #? Check if requrired files exists
 # The script wont work if these files are missing.
 # So we check if they exists, if not we exit the script.
-files=("request.token" "client_secrets.json" "$config_file")
-for file in "${files[@]}"; do
-	if [[ ! -f "$file" ]]; then
-		echo "$file is missing"
-		echo "Add/Create $file then run "pm2 restart procces_id_here" to restart the script."
-		exit 1
-	fi
-done
+if test -f request.token -a -f client_secrets.json -a -f "$config_file"; then
+	echo "All required files exist"
+else
+	echo "One or more required files are missing"
+	exit 1
+fi
 
 echo "Starting AutoVOD"
 echo "Loading $config_file"
@@ -46,46 +44,48 @@ while true; do
 	TIME_DATE=[$(date +"%m.%d.%y")]
 
 	getStreamInfo() {
-		#? Fetching stream metadata
-		# Using my own API to wrap around twitch's API to fetch additional stream metadata.
-		# Src code for this: https://github.com/jenslys/twitch-api-wrapper
-		echo "Trying to fetching stream metadata"
-		json=$(curl -s --retry 5 --retry-delay 2 --connect-timeout 30 $API_URL)
-		if [ "$json" = "Too many requests, please try again later." ]; then
-			echo $json
-			echo ""
-		fi
-
-		STREAMER_TITLE=$(echo "$json" | jq -r '.stream_title')
-		STREAMER_GAME=$(echo "$json" | jq -r '.stream_game')
-
-		if [ "$STREAMER_TITLE" = null ]; then
-			echo "Stream seems offline, can't fetch metadata."
-			echo ""
-		else
-			echo "Stream is online!"
-			echo "Current Title:"$STREAMER_TITLE
-			echo "Current Game:" $STREAMER_GAME
-			echo ""
-		fi
-	}
-
-	checkVariables() {
 		#? Checking if we need to fetch stream metadata.
-		# Checks if the variables contains the string "STREAMER_TITLE" or "STREAMER_GAME".
-		# if it does, we use the API to fetch the stream metadata.
-		# This check was added so we don't make unnenecesary API calls.
+		# Checks if the variables contain the string "STREAMER_TITLE" or "STREAMER_GAME".
+		# If it does, we use the API to fetch the stream metadata.
+		# These check were added so we don't make unnecessary calls to my API.
+		fetch_metadata=false
 		for var in "$@"; do
 			if [[ "$var" == *"$STREAMER_TITLE"* || "$var" == *"$STREAMER_GAME"* ]]; then
-				return 0
+				fetch_metadata=true
+				break
 			fi
 		done
 
-		return 1
+		if $fetch_metadata; then
+			#? Fetching stream metadata
+			# Using my own API to wrap around twitch's API to fetch additional stream metadata.
+			# Src code for this: https://github.com/jenslys/twitch-api-wrapper
+			echo "Trying to fetching stream metadata"
+			json=$(curl -s --retry 5 --retry-delay 2 --connect-timeout 30 $API_URL)
+			if [ "$json" = "Too many requests, please try again later." ]; then
+				echo "$json"
+				echo ""
+			fi
+
+			STREAMER_TITLE=$(echo "$json" | jq -r '.stream_title')
+			STREAMER_GAME=$(echo "$json" | jq -r '.stream_game')
+
+			if [ "$STREAMER_TITLE" = null ]; then
+				echo "Stream seems offline, can't fetch metadata."
+				echo ""
+				return 1
+			else
+				echo "Stream is online!"
+				echo "Current Title: "$STREAMER_TITLE
+				echo "Current Game: "$STREAMER_GAME
+				echo ""
+				return 0
+			fi
+		fi
 	}
 
-	if checkVariables "$VIDEO_TITLE" "$VIDEO_DESCRIPTION" "$VIDEO_PLAYLIST"; then
-		getStreamInfo $STREAMER_NAME STREAMER_TITLE STREAMER_GAME
+	if ! getStreamInfo "$VIDEO_TITLE" "$VIDEO_DESCRIPTION" "$VIDEO_PLAYLIST"; then
+		echo "Skipping fetching extra metadata"
 	fi
 
 	#? Splitting the stream into parts
