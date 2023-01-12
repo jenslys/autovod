79da88eded9983ffc12f023a0a4d6a79c6f9ac7a
Jens <69081683+jenslys@users.noreply.github.com>
2023-01-12 16:36:17 +0100

    Config file (#28)
    Co-authored-by: Manuel Schneider <57318033+BrainyXS@users.noreply.github.com>

--- AutoVOD.sh
+++ AutoVOD.sh
@@ -1,74 +1,123 @@
 #!/bin/bash
 
-echo "Starting AutoVOD..."
-echo "Using Twitch user: $TWITCH_USER"
-echo ""
-echo ""
+TIME_DATE='date +%m.%d.%y'
+TIME_CLOCK='date +%H:%M:%S'
 
-function getStreamInfo() {
-	# This uses my own API wrapper for twitch that i have hosted. (https://github.com/jenslys/twitch-api-wrapper).
-	# i would recommend self hosting yourself with your own API credentials. but you may use the one provided below.
-
-	echo "Fetching stream metadata..."
-	echo ""
-	url="https://twitch-api-wrapper.vercel.app/info/$1"
-	json=$(curl -s --retry 5 --retry-delay 2 --connect-timeout 30 $url)
-	if [ "$json" = "Too many requests, please try again later." ]; then
-		echo $json
-		echo ""
-		return
-	fi
-
-	STREAMER_TITLE=$(echo "$json" | jq -r '.stream_title')
-	STREAMER_GAME=$(echo "$json" | jq -r '.stream_game')
-
-	if [ "$json" = "[]" ]; then
-		echo "Stream is offline, can't fetch metadata."
-		echo ""
-	else
-		echo "Stream is online!"
-		echo "Current Title: $STREAMER_TITLE"
-		echo "Current Game: $STREAMER_GAME"
-		echo ""
-	fi
+# Function to get the value of the --name option
+fetch_args() {
+	# Parse command-line options
+	while getopts ":n:" opt; do
+		case $opt in
+		n)
+			name=$OPTARG
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument." >&2
+			exit 1
+			;;
+		esac
+	done
 }
 
-while true; do
-	STREAMER_NAME=$TWITCH_USER                                            #! Dont change this.
-	API_CALLS="false"                                                     #? Enable if you want to fetch stream metadata like the Title or Game. You can use the folowing variables with this enabled: $STREAMER_TITLE and $STREAMER_GAME.
-	if [[ "$API_CALLS" == "true" ]]; then                                 #
-		getStreamInfo $STREAMER_NAME STREAMER_TITLE STREAMER_GAME            #
-	fi                                                                    #
-	TIME_DATE=[$(date +"%m.%d.%y")]                                       # Preview example: [08.10.21]
-	VIDEO_VISIBILITY="unlisted"                                           #* Options: unlisted, private, public
-	VIDEO_DESCRIPTION="Uploaded using https://github.com/jenslys/AutoVOD" # YouTube video description.
-	VIDEO_TITLE="$STREAMER_NAME - $TIME_DATE"                             # Title of the Youtube video.
-	VIDEO_DURATION="12:00:00"                                             # XX:XX:XX (YouTube has a upload limit of 12 hours per video).
-	VIDEO_PLAYLIST="$STREAMER_NAME VODs"                                  # Playlist to upload to.
-	SPLIT_INTO_PARTS="false"                                              #? If you want to split the video into parts, set this to true. (if this is enabled VIDEO_DURATION is ignored).
-	SPLIT_VIDEO_DURATION="06:00:00"                                       # Duration of each part. (XX:XX:XX)
+# Call the function to get the value of the --name option
+fetch_args "$@"
+STREAMER_NAME=$name
+echo "Selected streamer: $STREAMER_NAME"
+config_file="$STREAMER_NAME.config"
 
-	# Splitting the stream into parts (If enabled)
+#? Check if requrired files exists
+# The script wont work if these files are missing.
+# So we check if they exists, if not we exit the script.
+if test -f request.token -a -f client_secrets.json -a -f "$config_file"; then
+	echo "All required files exist"
+else
+	echo "One or more required files are missing"
+	exit 1
+fi
+
+echo "Starting AutoVOD"
+echo "Loading $config_file"
+source $config_file
+echo ""
+
+while true; do
+	if [[ "$API_CALLS" == "true" ]]; then
+		#? Fetching stream metadata
+		# Using my own API to wrap around twitch's API to fetch additional stream metadata.
+		# Src code for this: https://github.com/jenslys/twitch-api-wrapper
+
+		# Store the orignal values
+		variables=("VIDEO_TITLE" "VIDEO_PLAYLIST" "VIDEO_DESCRIPTION")
+		for var in "${variables[@]}"; do
+			original_var=original_$var
+			eval "$original_var=\$$var"
+		done
+
+		echo "Trying to fetching stream metadata"
+		json=$(curl -s --retry 5 --retry-delay 2 --connect-timeout 30 $API_URL)
+		if [ "$json" = "Too many requests, please try again later." ]; then
+			echo "$json"
+			echo ""
+		else
+			FETCHED_TITLE=$(echo "$json" | jq -r '.stream_title')
+			FETCHED_GAME=$(echo "$json" | jq -r '.stream_game')
+		fi
+
+		if [ "$STREAMER_TITLE" = null ]; then
+			echo "Stream seems offline, can't fetch metadata."
+			echo ""
+		else
+			echo "Stream is online!"
+			echo "Current Title: ""$FETCHED_TITLE"
+			echo "Current Game: ""$FETCHED_GAME"
+
+			#? Replace the variables with the fetched metadata
+			for var in "${variables[@]}"; do
+				eval "$var=\${$var//\$STREAMER_TITLE/$FETCHED_TITLE}"
+				eval "$var=\${$var//\$STREAMER_GAME/$FETCHED_GAME}"
+			done
+
+			echo ""
+		fi
+	fi
+
+	#? Splitting the stream into parts
+	# Here we override the video_duratiation variable with the split_video_duration variable.
+	# We then compare the current date with the date from the last time we ran the script.
+	# if the date is the same, we add 1 to the current part variable.
+	# if the date is different, we reset the current part variable to 1.
 	if [[ "$SPLIT_INTO_PARTS" == "true" ]]; then
 		VIDEO_DURATION=$SPLIT_VIDEO_DURATION
-		if [[ "$TIME_DATE" == "$TIME_DATE_CHECK" ]]; then
+		if [[ "$($TIME_DATE)" == "$TIME_DATE_CHECK" ]]; then
+			# Increment the CURRENT_PART variable
 			CURRENT_PART=$(($CURRENT_PART + 1))
-			VIDEO_TITLE="$VIDEO_TITLE - Part $CURRENT_PART"
 		else
+			# Reset CURRENT_PART to 1 if the current date is not equal to TIME_DATE_CHECK
 			CURRENT_PART=1
 		fi
+		# Add " - Part $CURRENT_PART" to the end of the VIDEO_TITLE variable
+		VIDEO_TITLE="$VIDEO_TITLE - Part $CURRENT_PART"
 	fi
 
 	STREAMLINK_OPTIONS="best --hls-duration $VIDEO_DURATION --twitch-disable-hosting --twitch-disable-ads --twitch-disable-reruns -O --loglevel error" # https://streamlink.github.io/cli.html#twitch
 
-	echo "Checking twitch.tv/$STREAMER_NAME for a stream."
+	echo "Checking twitch.tv/"$STREAMER_NAME "for a stream"
 
 	# Create the input file with upload parameters
 	echo '{"title":"'"$VIDEO_TITLE"'","privacyStatus":"'"$VIDEO_VISIBILITY"'","description":"'"$VIDEO_DESCRIPTION"'","playlistTitles":["'"${VIDEO_PLAYLIST}"'"]}' >/tmp/input.$STREAMER_NAME
 
-	# Start StreamLink and YoutubeUploader
-	streamlink twitch.tv/$STREAMER_NAME $STREAMLINK_OPTIONS | youtubeuploader -metaJSON /tmp/input.$STREAMER_NAME -filename - >/dev/null 2>&1 && TIME_DATE_CHECK=$TIME_DATE
+	# Pass the stream from streamlink to youtubeuploader and then send the file to the void (dev/null)
+	streamlink twitch.tv/$STREAMER_NAME $STREAMLINK_OPTIONS | youtubeuploader -metaJSON /tmp/input.$STREAMER_NAME -filename - >/dev/null 2>&1 && TIME_DATE_CHECK=$($TIME_DATE)
+
+	# Restore the original values
+	for var in "${variables[@]}"; do
+		eval "$var=\$original_$var"
+	done
 
 	echo "Trying again in 1 minute"
-	sleep 1m
+	sleep 60
 done
